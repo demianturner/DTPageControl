da4049e12f34f489605fb1bcaf1a17356319f595
Demian Turner <demian@muse23.com>
2020-06-08 14:40:22 +0100

    Integrate example.

---
+++ Demo/PageControlExample/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PageControlExample
+//
+//  Created by Demian Turner on 01/05/2020.
+//  Copyright © 2020 Demian Turner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+}
+
---
+++ Demo/PageControlExample/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  PageControlExample
+//
+//  Created by Demian Turner on 01/05/2020.
+//  Copyright © 2020 Demian Turner. All rights reserved.
+//
+
+import Cocoa
+import DTPageControl
+
+class ViewController: NSViewController {
+    
+    private var pageControl: DTPageControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupPageControl()
+    }
+    
+    // MARK: - private
+
+    private func setupPageControl() {
+        pageControl = DTPageControl()
+        pageControl.numberOfPages = 4
+        let width: CGFloat = 200
+        let x: CGFloat = (view.frame.width - width) / 2
+        pageControl.frame = CGRect(x: x, y: 20, width: 200, height: 20)
+        view.addSubview(pageControl)
+    }
+
+    // MARK: - IBAction
+
+    @IBAction func tapPreviousButton(_ sender: NSButton) {
+        pageControl.currentPage -= 1
+    }
+
+    @IBAction func tapNextButton(_ sender: NSButton) {
+        pageControl.currentPage += 1
+    }
+}
+
